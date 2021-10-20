4d767046e4ed961b8eb09a8bdd5a9fa899eda678
zmd9220 <zmd9220@gmail.com>
2021-10-20 22:22:34 +0900

    211020 [MODIFY] : programmers 여행 경로 문제 해결

--- programmers_python/고득점 kit/dfs & bfs/여행경로.py
+++ programmers_python/고득점 kit/dfs & bfs/여행경로.py
@@ -1,16 +1,24 @@
 from collections import defaultdict, deque
 
-
 def dfs(dic, route, length):
+    # print('dic = ', dic)
+    # print('dic[route[-1] =', dic[route[-1]])
+    # print('route =',route)
+    # print(route[-1])
+    # print(len(dic[route[-1]]))
     if len(route) == length:
-        pass
-    for i in range(len(dic[route[-1]])):
-        tmp = dic[route[-1]].pop()
-        route.append(tmp)
-        dfs(dic, tmp)
-        route.pop()
-        dic[route[-1]].append(tmp)
-
+        global ans
+        ans.append(route[:])
+    elif dic[route[-1]]:
+        for i in range(len(dic[route[-1]])):
+            tmp = dic[route[-1]].pop(0)
+            # print('tmp', tmp)
+            route.append(tmp)
+            dfs(dic, route, length)
+            route.pop()
+            dic[route[-1]].append(tmp)
+    else:
+        return
 
 def dfs2(dic, route):
     answer = []
@@ -26,23 +34,29 @@ def solution(tickets):
     dic = defaultdict(list)
     for ticket in tickets:
         # dic[ticket[0]].setdefault(dic[ticket[0]].append(ticket[1]), [])
-        dic[ticket[0]].append([ticket[1], 0])
+        dic[ticket[0]].append(ticket[1])
     # print(dic, len(dic), len(tickets)+1)
     # print('ICN'>'ATL')
-    st = [['ICN']]
-    while st:
-        tmp = st.pop()
-        if len(tmp) == len(tickets) + 1:
-            answer.append(tmp)
-            continue
-        for i in range(len(dic[tmp[-1]])):
-            if not dic[tmp[-1]][i][1]:
-                tmp.append(dic[tmp[-1]][i][0])
-                dic[tmp[-1]][i][1] = 1
-                st.append(tmp)
-                tmp.pop()
-
-    print(answer)
-    # dfs(dic, ["ICN"], len(tickets) + 1)
-
+    # st = [['ICN']]
+    # while st:
+    #     tmp = st.pop()
+    #     if len(tmp) == len(tickets)+1:
+    #         answer.append(tmp)
+    #         continue
+    #     for i in range(len(dic[tmp[-1]])):
+    #         if not dic[tmp[-1]][i][1]:
+    #             tmp.append(dic[tmp[-1]][i][0])
+    #             dic[tmp[-1]][i][1] = 1
+    #             st.append(tmp)
+    #             tmp.pop()
+    # print(answer)
+    # print(dic)
+    dfs(dic, ["ICN"], len(tickets) + 1)
+    global ans
+    # print(ans)
+    ans.sort()
+    answer = ans[0]
+    ans = []
     return answer
+
+ans = []
