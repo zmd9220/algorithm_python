0b7d69fb3a04c63070fcb2b5b2bd2410639e88ed
zmd9220 <zmd9220@gmail.com>
2021-11-10 23:25:01 +0900

    211110 [MODIFY] : 큐 2개로 스택 만드는 부분 로직이 잘못되어 수정

--- hand_coding/큐2개로 스택.py
+++ hand_coding/큐2개로 스택.py
@@ -8,20 +8,34 @@ class stack:
 
     def __init__(self, arr):
         for item in arr:
-            self.q1.put(item)
-        while self.q1:
-            self.q2.put(self.q1.get())
+            self.q1.append(item)
+            while self.q2:
+                # self.q1.put(self.q2.get())
+                self.q1.append(self.q2.popleft())
+            # self.q1.put(item)
+            # self.q1.append(item)
+            print(self.q1)
+
+            while self.q1:
+                # self.q2.put(self.q1.get())
+                self.q2.append(self.q1.popleft())
+            print(self.q2)
 
     def push(self, num):
+        self.q1.append(num)
         while self.q2:
-            self.q1.put(self.q2.get())
-        self.q1.put(num)
+            # self.q1.put(self.q2.get())
+            self.q1.append(self.q2.popleft())
+        # self.q1.put(num)
+        # self.q1.append(num)
         while self.q1:
-            self.q2.put(self.q1.get())
+            # self.q2.put(self.q1.get())
+            self.q2.append(self.q1.popleft())
         print(self.q2)
 
     def pop(self):
-        return self.q2.get()
+        # return self.q2.get()
+        return self.q2.popleft()
 
 
 arr = [1, 3, 5, 7, 9]
